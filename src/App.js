704049ca7acdf3f9e1c54cdc92a9c97ca7cb122e
James Randolph <james.randolph2016@gmail.com>
2020-10-08 08:36:47 -0400

    addorbitcontrols

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Canvas } from "react-three-fiber";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { Canvas, useThree, extend } from "react-three-fiber";
 import './App.css';
+
+extend({  OrbitControls })
 
 function Cube(props) {
   return (
@@ -14,12 +17,22 @@ function Cube(props) {
 // args = [width, height, depth]
 
 function Scene() {
+
+  const {
+    camera,
+    gl: {
+      domElement
+    }
+  } = useThree()
+
   return (
     <>
       <ambientLight />
       <pointLight  intensity={0.5} position={[-1,2,4]} />
       <Cube rotation={[10,10,0]} position={[0,0,0]}/>
       <Cube rotation={[10,20,0]} position={[2,2,0]}/>
+
+      <orbitControls args={[ camera, domElement ]} />
     </>
   )
 }
