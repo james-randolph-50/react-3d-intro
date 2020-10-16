b6d3bdf5886fb74d9230f8794a4c355c9960e225
James Randolph <james.randolph2016@gmail.com>
2020-10-16 18:03:15 -0400

    1000 cube matrix  added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,25 @@ import { OrbitControls } from 'drei';
 import { Canvas, useFrame } from "react-three-fiber";
 import './App.css';
 
+const tempObject = new THREE.Object3D();
+
 function  Boxes() {
 
   const ref = useRef();
+
+  useFrame(() => {
+    let i = 0;
+    for (let x = 0; x <  10; x++) {
+      for (let y = 0; y <  10; y++) {
+        for (let z = 0; z <  10; z++) {
+          const id = i++;
+          tempObject.position.set(5-x, 5-y, 5-z);
+          tempObject.updateMatrix();
+          ref.current.setMatrixAt(id, tempObject.matrix);
+        }
+      }
+    }
+  });
 
   return (
     <instancedMesh ref={ref} args={[null, null, 1000]}>
